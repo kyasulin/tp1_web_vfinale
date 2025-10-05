b1150d686d4db43d8aeee664ac6a2b4986d5efc8
Kath <kathleenvjeanjacques@gmail.com>
2025-10-05 02:26:34 -0400

    crash-out 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
-document.addEventListener("shown.bs.modal", function () {
+document.addEventListener("bs.modal", function () {
   console.log("Le modal de la bande-annonce est ouvert !");
 });
+
+/* je ne peux pas utiliser javascript cries- bruh */
